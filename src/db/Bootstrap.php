eb91be6ed05f30b86a643292d60b42941f3ffe3a
santilin@securex <franciscocapel@cepaim.org>
2022-06-08 12:19:09 +0200

    Param to disable foreign-key checks in boostrap

--- src/db/Bootstrap.php
+++ src/db/Bootstrap.php
@@ -17,8 +17,8 @@ class Bootstrap implements BootstrapInterface
     public function bootstrap($app)
     {
 		\Yii::$classMap['yii\db\sqlite\QueryBuilder'] = "@santilin/db/sqlite/QueryBuilder.php";
-// 		\Yii::$classMap['yii\db\sqlite\Schema'] = "@santilin/db/sqlite/Schema.php";
-		if( isset(Yii::$app->db) ) {
+		if( (Yii::$app->params['diable_foreign_key_checks']??true)
+			&& isset(Yii::$app->db) ) {
 			Yii::$app->db->on(Connection::EVENT_AFTER_OPEN, function($e) {
 				Yii::$app->db->createCommand()->checkIntegrity(true)->execute();
 			});
