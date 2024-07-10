d716484e14f8c3604a94b3238dead233c6cbd707
Santilín <software@noviolento.es>
2024-07-10 10:13:53 +0200

    CONCAT => ||    Coalesce field values to avoid getting a null string if any of the fields is null

--- src/db/sqlite/ExpressionBuilder.php
+++ src/db/sqlite/ExpressionBuilder.php
@@ -36,26 +36,28 @@ class ExpressionBuilder implements ExpressionBuilderInterface
 			return "strftime('%Y-%m-%d %H:%M:%f', 'now')";
 		} else if ($value == "UNIX_TIMESTAMP()") {
 			return "CAST(strftime('%s', 'now') AS INT)";
-		} else {
-			$matches = null;
-			if( preg_match_all("/(.*)\bCONCAT\b\((.*?)\)(.*)/", $expression, $matches) ) {
-				$fields = $matches[2][0];
-  				if( preg_match_all(<<<regexp
+		} else if( preg_match_all("/(.*)\bCONCAT\b\((.*?)\)(.*)/", $expression, $matches) ) {
+			$fields = $matches[2][0];
+			if( preg_match_all(<<<regexp
 /\s*([^'`,]+)\s*|\s*['`]([^'`]+)['`]\s*/
 regexp
-					, $fields, $fld_matches)) {
-					$fields = []; // Adds ` to field names wihtout quotes
-					foreach ($fld_matches[0] as $k => $v ) {
-						$v = trim($v);
-						if( $v != '' ) {
+				, $fields, $fld_matches)) {
+				$fields = []; // Adds ` to field names wihtout quotes
+				foreach ($fld_matches[0] as $k => $v ) {
+					$v = trim($v);
+					if( $v != '' ) {
+						// must coallesce fields to '' to avoid getting a whole null string
+						if (preg_match('/("[a-zA-Z_]([a-zA-Z0-9_]*)")|([a-zA-Z_]([a-zA-Z0-9_]*))/', $v)) {
+							$fields[] = "COALESCE($v, '')";
+						} else {
 							$fields[] = $v;
 						}
 					}
-					return ($matches[1][0] . join('||', $fields) . $matches[3][0]);
 				}
-			} else if( preg_match_all("/(.*)\bGROUP_CONCAT\b\((.*?)\bSEPARATOR\b(.*)\)/", $expression, $matches) ) {
-				return "GROUP_CONCAT({$matches[2][0]}, {$matches[3][0]})";
+				return ($matches[1][0] . join('||', $fields) . $matches[3][0]);
 			}
+		} else if( preg_match_all("/(.*)\bGROUP_CONCAT\b\((.*?)\bSEPARATOR\b(.*)\)/", $expression, $matches) ) {
+			return "GROUP_CONCAT({$matches[2][0]}, {$matches[3][0]})";
 		}
 		return $value;
 	}
